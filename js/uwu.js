0ac08598961f62e707194e897f66bffbe00450ad
NafisMuhammadSabiq <nafisharist@gmail.com>
2025-10-21 15:31:13 +0700

    Change audio file from 'uwu.mp3' to 'Dewa.mp3'

--- js/uwu.js
+++ js/uwu.js
@@ -2,7 +2,7 @@ const s = (p) => {
   let demo1Shader, img, fft, audio, toggleBtn
 
   p.preload = () => {
-    audio = p.loadSound('audio/uwu.mp3')
+    audio = p.loadSound('audio/Dewa.mp3')
     demo1Shader = p.loadShader('shaders/base.vert', 'shaders/d1.frag')
     img = p.loadImage('img/1.jpg')
   }
@@ -63,5 +63,6 @@ const s = (p) => {
     }
   }
 };
+
 
 new p5(s)
